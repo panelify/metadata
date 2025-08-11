08191916bca25e77c8d0819ad1d6845a6db3d5b3
majidmohammadian <majeedmohammadian@gmail.com>
2025-08-11 20:31:43 +0330

    added translation file

---
+++ lang/en/base.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Panelify Metadata Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Metadata for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    "components" => [
+        'metadata_card' => [
+            'title' => 'Additional Information',
+        ],
+    ],
+
+];
